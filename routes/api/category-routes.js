fb8e335d2f07ce48f2d96d45a92066d2776e9df3
Sona Shrestha <sona.shrestha004@gmail.com>
2021-05-28 22:21:38 +1000

    adding new category

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,11 +17,11 @@ router.get("/", async (req, res) => {
   }
 });
 
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = Category.findByPk(req.params.id, {
+    const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product, attributes: ["category_id"] }],
     });
 
@@ -36,8 +36,14 @@ router.get("/:id", (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err.message);
+  }
 });
 
 router.put("/:id", (req, res) => {
