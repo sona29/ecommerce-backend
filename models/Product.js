a9daa794b0d0dec9a43ba8abbe3bd39370236ee2
Sona Shrestha <sona.shrestha004@gmail.com>
2021-05-28 18:26:24 +1000

    adding references to category id

--- models/Product.js
+++ models/Product.js
@@ -33,6 +33,11 @@ Product.init(
     },
     category_id: {
       type: DataTypes.INTEGER,
+      references: {
+        model: "category",
+        key: "id",
+        unique: false,
+      },
     },
   },
   {
