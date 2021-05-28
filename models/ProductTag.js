3117cf6b58e18a5c90a6b4bb9169b6e9bc6dc08b
Sona Shrestha <sona.shrestha004@gmail.com>
2021-05-28 18:29:14 +1000

    adding foreign key references

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -15,9 +15,19 @@ ProductTag.init(
     },
     product_id: {
       type: DataTypes.INTEGER,
+      references: {
+        model: "product",
+        key: "id",
+        unique: false,
+      },
     },
     tag_id: {
       type: DataTypes.INTEGER,
+      references: {
+        model: "tag",
+        key: "id",
+        unique: false,
+      },
     },
   },
   {
