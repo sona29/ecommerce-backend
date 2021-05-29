194e57b7db074df7a553e405eb054956031eb07f
Sona Shrestha <sona.shrestha004@gmail.com>
2021-05-30 09:37:39 +1000

    adding route to view product by PK

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -21,9 +21,26 @@ router.get("/", async (req, res) => {
 });
 
 // get one product
-router.get("/:id", (req, res) => {
+router.get("/:id", async (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  try {
+    const productData = await Product.findByPk(req.params.id, {
+      include: [
+        { model: Category, attributes: ["category_name"] },
+        { model: Tag, attributes: ["tag_name"] },
+      ],
+    });
+
+    if (!productData) {
+      res.status(404).json({ message: "No category found with this id!" });
+      return;
+    }
+
+    res.status(200).json(productData);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 });
 
 // create new product
