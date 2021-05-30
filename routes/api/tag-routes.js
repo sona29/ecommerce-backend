f95fd1f44c9f2b6140c6791ede3b1c1297e39b75
Sona Shrestha <sona.shrestha004@gmail.com>
2021-05-30 10:21:30 +1000

    adding route to add and update tag

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -39,12 +39,39 @@ router.get("/:id", async (req, res) => {
   }
 });
 
-router.post("/", (req, res) => {
+router.post("/", async (req, res) => {
   // create a new tag
+  try {
+    const tagData = await Tag.create({ tag_name: req.body.tag_name });
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(400).json(err.message);
+  }
 });
 
-router.put("/:id", (req, res) => {
+router.put("/:id", async (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(
+      {
+        tag_name: req.body.tag_name,
+      },
+      {
+        where: {
+          id: req.params.id,
+        },
+      }
+    );
+
+    if (!tagData) {
+      res.status(404).json({ message: "No category found with this id!" });
+      return;
+    }
+
+    res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err.message);
+  }
 });
 
 router.delete("/:id", (req, res) => {
